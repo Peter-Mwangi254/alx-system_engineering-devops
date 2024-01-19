db48a9b888f621a9827a123b9eaa8249c19a24e7
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-20 00:08:57 +0300

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+}
