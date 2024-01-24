90300df63fe4aadf4ea9a3137cbea570ac442226
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-25 00:57:45 +0300

    configuring nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,7 +8,7 @@ file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Peter-Mwangi254 permanent;',
 }
 
 file { '/var/www/html/index.html':
