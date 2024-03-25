222d35293fc8f8ddae54113a38a7183eb6bbddb2
Peter-Mwangi254 <pm6093628@gmail.com>
2024-03-25 13:42:36 -0700

    export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to JSON format."""
 
+import json
 import requests
 import sys
-import json
 
 
 if __name__ == '__main__':
